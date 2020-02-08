3eb16b07992dace15b03c37292e85245f10f008c
jason <ifrit98@gmail.com>
2020-02-07 21:32:14 -0500

    update img_gen

--- .Rprofile
+++ .Rprofile
@@ -15,19 +15,3 @@ suppressPackageStartupMessages(library(JSGutils))
 source("labels.R")
 source("misc.R")
 source_python("python/data_tools.py")
-
-
-# TRAIN <- list(
-#   'data/data-raw/train_image_data_0.parquet',
-#   'data/data-raw/train_image_data_1.parquet',
-#   'data/data-raw/train_image_data_2.parquet',
-#   'data/data-raw/train_image_data_3.parquet'
-# )
-# 
-# TEST <-  list(
-#   'data/data-raw/test_image_data_0.parquet',
-#   'data/data-raw/test_image_data_1.parquet',
-#   'data/data-raw/test_image_data_2.parquet',
-#   'data/data-raw/test_image_data_3.parquet'
-# )
-
--- python/tfrecords/img_generator.py
+++ python/tfrecords/img_generator.py
@@ -18,12 +18,20 @@ BATCH = 3
 
 
 test_df = pd.merge(
-  pd.read_parquet(f'data/data-raw/test_image_data_{i}.parquet'),
+  pd.read_parquet(f'data/data-raw/test_image_data_{0}.parquet'),
   test_df_, on='image_id'
 ).drop(['image_id'], axis=1)
 
 
-df = pd.read_parquet(f'data/data-raw/test_image_data_{i}.parquet')
+df0 = pd.read_parquet(f'data/data-raw/test_image_data_{0}.parquet')
+# df1 = pd.read_parquet(f'data/data-raw/test_image_data_{1}.parquet')
+# df2 = pd.read_parquet(f'data/data-raw/test_image_data_{2}.parquet')
+# df3 = pd.read_parquet(f'data/data-raw/test_image_data_{3}.parquet')
+
+
+# df = df0.merge(df1)
+df = df0
+
 
 def generator(df):
   # grab only pixel values
@@ -31,10 +39,14 @@ def generator(df):
 
   # iterate over pandas df 
   for idx, row in df.iterrows():
-    if idx % 3 == 0:
+    if idx % BATCH == 0:
       pass
   # create batch
-  batch = {"image" : np.stack([df.iloc[i][1:].astype(np.int16).values 
-                        for i in range(BATCH)], 0)}
+  batch = {'image': np.stack([df.iloc[i][1:].astype(np.int16).values 
+                        for i in range(BATCH)], 0),
+           'grapheme': ,
+           'vowel':,
+           'consonant':}
 
   
+# Split up into train/test data.  The "test_df_" is left blank intentionally. No labels.
