d7422d6b3b5068e290de3647928519688de7ead9
jason <ifrit98@gmail.com>
2020-02-15 23:38:24 -0500

    add large conv model

--- flags.R
+++ flags.R
@@ -1,11 +1,12 @@
 IMGSIZE = 128
 
 FLAGS <- tensorflow::flags(
-  flag_integer("epochs", 200),
+  flag_integer("epochs", 250),
   flag_integer("steps_per_epoch", 250),
   flag_integer("height", IMGSIZE),
   flag_integer("width", IMGSIZE),
   flag_integer("batch_size", 32),
   flag_integer("val_size", 100),
+  flag_boolean("global_pool", TRUE),
   flag_string("source_dir", "data/data-tfrecord-norm/")
 )
--- train.R
+++ train.R
@@ -6,7 +6,9 @@ if (!exists_here("FLAGS")) {
 
 # source("dataset-npz.R")
 import_from("dataset.R", ds, val_ds)
-import_from("models/base.R", model)
+# TODO: Add find model functionality from JSGutils/deepR
+# import_from("models/base.R", model)
+import_from("models/conv-simple.R", model)
 
 
 callbacks <- #list()
@@ -14,6 +16,7 @@ callbacks <- #list()
     callback_reduce_lr_on_plateau(monitor = "grapheme_root_loss"),
     callback_reduce_lr_on_plateau(monitor = "consonant_loss"),
     callback_reduce_lr_on_plateau(monitor = "vowel_loss"),
+    callback_early_stopping(monitor = "val_grapheme_root_loss", patience = 25),
     callback_model_checkpoint("model-weights-best-checkpoint.h5", monitor = "grapheme_root_acc"),
     callback_tensorboard(file.path("logs", stringr::str_squish(lubridate::now())))
   )
@@ -34,3 +37,6 @@ plot(hist)
 qs::qsave(hist, "history.qs")
 
 # TODO: investigate images after reading TFRECORDS -> MAKE SURE THEY ARE LEGIT
+# TODO: Try CLR in different modes.  [Triangular, Exponential]
+# TODO: Add tfruns functionality to launch from terminal**
+# TODO: 
